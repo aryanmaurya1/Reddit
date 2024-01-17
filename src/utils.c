e394e544230a0f7ae778b6035525d30070bc25a6
aryanmaurya1 <aryanmaurya1@outlook.com>
2024-01-18 02:08:21 +0530

    code refactoring

--- src/utils.c
+++ src/utils.c
@@ -44,7 +44,9 @@ void run(GLFWwindow *window)
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
         loop(VAO, shaderProgram);
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
